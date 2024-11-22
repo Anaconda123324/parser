ba3d84f9c0e884171f15d42ab8d71d146aa5a0b4
Anaconda123324 <alina.nozdrina.98@gmail.com>
2024-11-22 18:20:10 +0000

    +

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
-    url = 'https://www.52shuku.vip/yanqing/hx4r_2828.html'
+    url = 'https://tl.rulate.ru/book/16292/327323/ready_new'
     response = requests.get(url)
     print(response.status_code)
-    with open('output.html', 'a', encoding='UTF-8') as file:
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    part = soup.find('div', class_='text-container')
+    # print(part.text)
+    with open('page.txt', 'a', encoding='UTF-8') as file:       #достать сам текст из контейнера
+        file.write(part.text)
+
+    with open('output.html', 'a', encoding='UTF-8') as file:        #достать страницу
         file.write(response.text)
 
 
